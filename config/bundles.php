100e888e352a6a7d54174fd5f4b3ed384933ffc9
Markus Veith <mail@markusveith.de>
2017-10-06 20:17:16 +0200

    Initial Commit

--- config/bundles.php
+++ config/bundles.php
@@ -9,6 +9,4 @@ return [
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
